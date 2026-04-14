acd209945a4021d8681639d6fabc966d2d4b9bc4
rcp-solutions <lham@netplug.me>
2026-04-14 15:54:57 -0600

    Remove unused HMAC secret generation script

--- make-hmac-secret.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-const secret = 'Af1c3b708OkScMWM5EQ7Byv+FJfs8X9nlUZWN7cZFxc=';
-
-const message = 'your_request_payload';
-
-const signature = crypto.createHmac('sha256', secret).update(message).digest('hex');
-
-console.log(signature);
