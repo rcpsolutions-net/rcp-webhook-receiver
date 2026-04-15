f38b826d9e23a34306da19b9935a8d221ea81d92
rcp-solutions <lham@netplug.me>
2026-04-15 14:47:57 -0600

    Remove unnecessary await from MongoDB connection in webhook route

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -112,7 +112,7 @@ async function webhookRoutes(fastify) {
   );
 }
 
-await connect().catch((err) => {
+connect().catch((err) => {
   console.error("Failed to connect to MongoDB:", err);
   process.exit(1);
 });
